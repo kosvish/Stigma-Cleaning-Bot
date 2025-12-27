ddad422286bd2edf2ffbd59f5c52954977120312
mrkoc <mr.kocta@bk.ru>
2025-12-27 11:37:43 +0300

    v3

--- main.py
+++ main.py
@@ -26,10 +26,4 @@ def main():
 
 
 if __name__ == "__main__":
-    import subprocess
-    import sys
-
-    subprocess.check_call([
-        sys.executable, "-m", "pip", "install", "-r", "requirements.txt"
-    ])
     main()
